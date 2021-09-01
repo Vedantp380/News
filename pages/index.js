c1ce0299d129acb7910443c382fc84dd59c676c8
Abhishek Rana <abhishek.rana@mantralabsglobal.com>
2021-09-01 23:30:51 +0530

    [ADD] components and many more files edited

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Toolbar} from '../components/toolbar'
 
 export default function Home() {
   return(
-    <>Hello World</>
+    <div className = 'page-container'>
+     <Toolbar /> 
+
+      <div className = {styles.main}></div>
+        <h1>Mine Next.js App</h1>
+
+        <h3> Your one stop shop for the latest news articles</h3>
+
+    </div>
   )
   
 }
